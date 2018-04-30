07da152658e20083fc3ded7b9c8dc410c07cdab1
Francisco Morales <francisco.morales@intelligenia.com>
2018-04-30 13:32:48 +0200

    Return empty array if posts is None

--- last_social_activity/social_networks/facebook.py
+++ last_social_activity/social_networks/facebook.py
@@ -54,6 +54,9 @@ class FacebookReader(object):
 				return SocialNetworkItemCache.get("facebook", num_posts).response_dict
 			return []
 
+		if posts is None:
+			return []
+
 		for post in posts:
 			post['created_at'] = parser.parse(post.get('created_time')).isoformat()
 
